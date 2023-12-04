2799059355e386196c36df733b87c903c349e53b
Jake Goulding <jake.goulding@gmail.com>
2023-12-04 08:43:06 -0500

    Day 4 - Part 2

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -11,31 +11,17 @@ fn main() -> Result<u64, ParseError> {
     // Part 1: 25004
     println!("{sum}");
 
+    let number = number_of_scratchcards(INPUT)?;
+    // Part 2: 14427616
+    println!("{number}");
+
     Ok(())
 }
 
 fn sum_of_winning_points(s: &str) -> Result<u64, ParseError> {
     s.lines()
-        .map(|line| -> Result<_, ParseError> {
-            let mut parts = line.splitn(3, &[':', '|']);
-            let _id = parts.next().context(MissingIdSnafu { line })?;
-            let winners = parts.next().context(MissingWinnersSnafu { line })?;
-            let numbers = parts.next().context(MissingNumbersSnafu { line })?;
-
-            let winners = winners
-                .split_ascii_whitespace()
-                .map(|winner| winner.parse::<Num>().context(ParseWinnerSnafu { winner }))
-                .collect::<Result<BTreeSet<_>, _>>()
-                .context(InvalidWinnerSnafu { line })?;
-
-            let numbers = numbers
-                .split_ascii_whitespace()
-                .map(|number| number.parse::<Num>().context(ParseNumberSnafu { number }));
-
-            let n_matches = itertools::process_results(numbers, |numbers| {
-                numbers.filter(|n| winners.contains(n)).count()
-            })
-            .context(InvalidNumberSnafu { line })?;
+        .map(|line| {
+            let n_matches = n_matches(line)?;
 
             let points = if n_matches == 0 {
                 0
@@ -46,6 +32,47 @@ fn sum_of_winning_points(s: &str) -> Result<u64, ParseError> {
             Ok(points)
         })
         .sum()
+}
+
+fn number_of_scratchcards(s: &str) -> Result<usize, ParseError> {
+    let matches = s.lines().map(n_matches).collect::<Result<Vec<_>, _>>()?;
+    let mut total_count = vec![1; matches.len()];
+
+    for (idx, &matches) in matches.iter().enumerate() {
+        let current_count = total_count[idx];
+
+        for idx in (idx..).take(matches) {
+            // Adding one to get the card after the current
+            if let Some(total) = total_count.get_mut(idx + 1) {
+                *total += current_count;
+            }
+        }
+    }
+
+    Ok(total_count.into_iter().sum())
+}
+
+fn n_matches(line: &str) -> Result<usize, ParseError> {
+    let mut parts = line.splitn(3, &[':', '|']);
+
+    let _id = parts.next().context(MissingIdSnafu { line })?;
+    let winners = parts.next().context(MissingWinnersSnafu { line })?;
+    let numbers = parts.next().context(MissingNumbersSnafu { line })?;
+
+    let winners = winners
+        .split_ascii_whitespace()
+        .map(|winner| winner.parse::<Num>().context(ParseWinnerSnafu { winner }))
+        .collect::<Result<BTreeSet<_>, _>>()
+        .context(InvalidWinnerSnafu { line })?;
+
+    let numbers = numbers
+        .split_ascii_whitespace()
+        .map(|number| number.parse::<Num>().context(ParseNumberSnafu { number }));
+
+    itertools::process_results(numbers, |numbers| {
+        numbers.filter(|n| winners.contains(n)).count()
+    })
+    .context(InvalidNumberSnafu { line })
 }
 
 #[derive(Debug, Snafu)]
@@ -95,6 +122,14 @@ mod test {
     #[snafu::report]
     fn example_1() -> Result<(), ParseError> {
         assert_eq!(13, sum_of_winning_points(EXAMPLE_INPUT_1)?);
+
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_2() -> Result<(), ParseError> {
+        assert_eq!(30, number_of_scratchcards(EXAMPLE_INPUT_1)?);
 
         Ok(())
     }
