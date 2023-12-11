c7e207876aae67f29ab873f47460823f27777621
Jake Goulding <jake.goulding@gmail.com>
2023-12-11 08:35:36 -0500

    Day 11 - Part 2

--- day-11/src/main.rs
+++ day-11/src/main.rs
@@ -5,14 +5,18 @@ const INPUT: &str = include_str!("../input");
 
 #[snafu::report]
 fn main() -> Result<(), Error> {
-    let sum = sum_of_shortest_paths(INPUT)?;
+    let sum = sum_of_shortest_paths::<2>(INPUT)?;
     // Part 1: 9522407
+    println!("{sum}");
+
+    let sum = sum_of_shortest_paths::<1_000_000>(INPUT)?;
+    // Part 2:
     println!("{sum}");
 
     Ok(())
 }
 
-fn sum_of_shortest_paths(s: &str) -> Result<usize, Error> {
+fn sum_of_shortest_paths<const RATE: usize>(s: &str) -> Result<usize, Error> {
     let mut galaxies = Vec::new();
     let mut seen_columns = BTreeSet::new();
     let mut seen_rows = BTreeSet::new();
@@ -42,8 +46,8 @@ fn sum_of_shortest_paths(s: &str) -> Result<usize, Error> {
             let x_expansion = (x0..=x1).filter(|x| !seen_columns.contains(x)).count();
             let y_expansion = (y0..=y1).filter(|y| !seen_rows.contains(y)).count();
 
-            let x = x_base + x_expansion;
-            let y = y_base + y_expansion;
+            let x = x_base + x_expansion * (RATE - 1);
+            let y = y_base + y_expansion * (RATE - 1);
 
             distance_sum += x + y;
         }
@@ -71,7 +75,16 @@ mod test {
     #[test]
     #[snafu::report]
     fn example_1() -> Result<(), Error> {
-        assert_eq!(374, sum_of_shortest_paths(EXAMPLE_INPUT_1)?);
+        assert_eq!(374, sum_of_shortest_paths::<2>(EXAMPLE_INPUT_1)?);
+
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_2() -> Result<(), Error> {
+        assert_eq!(1030, sum_of_shortest_paths::<10>(EXAMPLE_INPUT_1)?);
+        assert_eq!(8410, sum_of_shortest_paths::<100>(EXAMPLE_INPUT_1)?);
 
         Ok(())
     }
