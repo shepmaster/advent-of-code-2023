d2a717ce9eeb2ed7e523b3b52b5d6f2d8d4675ad
Jake Goulding <jake.goulding@gmail.com>
2023-12-20 08:25:46 -0500

    Day 18 - Part 2

--- day-18/src/main.rs
+++ day-18/src/main.rs
@@ -1,6 +1,9 @@
-use core::fmt;
+use itertools::Itertools;
 use snafu::prelude::*;
-use std::{cmp, collections::BTreeSet, str::FromStr};
+use std::{
+    collections::{BTreeMap, BTreeSet},
+    str::FromStr,
+};
 
 const INPUT: &str = include_str!("../input");
 
@@ -12,21 +15,251 @@ fn main() -> Result<(), Error> {
     //         61865
     println!("{volume}");
 
+    let volume = lava_volume_fixed(INPUT)?;
+    // Part 2: 40343619199142
+    println!("{volume}");
+
     Ok(())
 }
 
-fn lava_volume(s: &str) -> Result<usize, Error> {
-    let steps = s
+fn lava_volume(s: &str) -> Result<i64, Error> {
+    let steps: Vec<_> = s
         .lines()
-        .map(|step| Step::try_from(step).context(StepSnafu { step }));
+        .map(|step| step.try_into().context(StepSnafu { step }))
+        .collect::<Result<_, _>>()?;
 
-    let map = itertools::process_results(steps, |steps| build_outline(steps)).unwrap();
+    Ok(points_of_interest_volume(steps))
+}
 
-    let bounds = Bounds::find(&map).context(BoundsSnafu)?;
+fn lava_volume_fixed(s: &str) -> Result<i64, Error> {
+    let steps: Vec<_> = s
+        .lines()
+        .map(|step| {
+            step.try_into()
+                .context(StepSnafu { step })
+                .map(|s: Step<'_>| s.fixup())
+        })
+        .collect::<Result<_, _>>()?;
 
-    let next = fill_in_outline(&map, bounds);
+    Ok(points_of_interest_volume(steps))
+}
 
-    Ok(next.len())
+#[derive(Debug, Copy, Clone)]
+enum Corner {
+    Lower,
+    Upper,
+}
+
+// Idea: Find all corners in the graph. That point, and the 8
+// immediate neighbors, are the only relevant points in the entire
+// graph.
+//
+// Take those unique X and Y coordinates and build a number of
+// rectanges that cover the entire graph.
+//
+// For each of those rectangles, pick a representative point inside
+// and check to see if it is a corner or a line. If so, they are
+// filled. Otherwise, cast a ray from the left, counting how many
+// vertical walls are intersected. An odd number means we are inside,
+// even means outside. We also have to track the number and kind of
+// corners to know if the twist mans we are inside or not.
+fn points_of_interest_volume(steps: Vec<Step<'_>>) -> i64 {
+    use direction_shorthand::*;
+    use Corner::*;
+
+    let prev_dirs = steps.iter().map(|s| s.dir).cycle().skip(steps.len() - 1);
+
+    let mut x = 0;
+    let mut y = 0;
+
+    let mut linear = 0;
+
+    let mut corners = BTreeMap::new(); // coord -> kind of corner
+    let mut v_lines = BTreeMap::new(); // x -> y0..=y1
+    let mut h_lines = BTreeMap::new(); // y -> x0..=x1
+
+    for (step, prev_dir) in steps.iter().zip(prev_dirs) {
+        let Step { dir, count, .. } = step;
+
+        // Count how long the outline is to check our math.
+        linear += count;
+
+        // Add corners.
+        let corner = match (prev_dir, dir) {
+            (U, R) => Lower,
+            (U, L) => Lower,
+
+            (R, U) => Upper,
+            (R, D) => Lower,
+
+            (D, R) => Upper,
+            (D, L) => Upper,
+
+            (L, U) => Upper,
+            (L, D) => Lower,
+
+            (U, U) | (R, R) | (D, D) | (L, L) => unreachable!("Lines cannot continue"),
+
+            (U, D) | (R, L) | (D, U) | (L, R) => unreachable!("Lines cannot reverse"),
+        };
+        corners.insert((x, y), corner);
+
+        let start_c = (x, y);
+
+        // Move our coordinate
+        match dir {
+            U => y -= count,
+            R => x += count,
+            D => y += count,
+            L => x -= count,
+        }
+
+        let end_c = (x, y);
+
+        // Take two endpoints of a line and return a range
+        // representing the range of the line exclusing the corners.
+        let inner_line = |v0, v1| {
+            let mut vs = [v0, v1];
+            vs.sort();
+            let [v0, v1] = vs;
+
+            (v0 + 1)..=(v1 - 1)
+        };
+
+        match dir {
+            U | D => {
+                v_lines
+                    .entry(x)
+                    .or_insert_with(Vec::new)
+                    .push(inner_line(start_c.1, end_c.1));
+            }
+            L | R => {
+                h_lines
+                    .entry(y)
+                    .or_insert_with(Vec::new)
+                    .push(inner_line(start_c.0, end_c.0));
+            }
+        }
+    }
+
+    // The locations of the corners and all 8 immediate neighbors.
+    const DELTAS: [i64; 3] = [-1, 0, -1];
+
+    let pois = corners.keys().flat_map(|&(x, y)| {
+        DELTAS
+            .into_iter()
+            .cartesian_product(DELTAS)
+            .map(move |(dx, dy)| (x + dx, y + dy))
+    });
+
+    let (xs, ys): (BTreeSet<_>, BTreeSet<_>) = pois.unzip();
+
+    // Pair up consecutive points to create the top-left and
+    // bottom-right points of each square.
+    let xs = xs.iter().copied().tuple_windows::<(_, _)>();
+    let ys = ys.iter().copied().tuple_windows::<(_, _)>();
+    let squares = xs.cartesian_product(ys);
+
+    let mut outline_filled = 0;
+    let mut inside_filled = 0;
+
+    for ((x0, x1), (y0, y1)) in squares {
+        let w = x1 - x0;
+        let h = y1 - y0;
+        let a = w * h;
+
+        // let top_left = (x0, y0);
+        let bottom_right = (x1, y1);
+
+        // The top-left point doesn't work, but the bottom-right does?
+        // A fencepost problem?
+        let rep = bottom_right;
+
+        let is_on_corner = || corners.contains_key(&rep);
+
+        let is_on_v_line = || {
+            v_lines
+                .iter()
+                .any(|(&x, ys)| x == rep.0 && ys.iter().any(|ys| ys.contains(&rep.1)))
+        };
+
+        let is_on_h_line = || {
+            h_lines
+                .iter()
+                .any(|(&y, xs)| y == rep.1 && xs.iter().any(|xs| xs.contains(&rep.0)))
+        };
+
+        if is_on_corner() {
+            assert_eq!(1, a, "a corner should only ever be exactly one square");
+            outline_filled += a;
+        } else if is_on_v_line() || is_on_h_line() {
+            outline_filled += a;
+        } else {
+            use Intersection::*;
+
+            #[derive(Debug, Copy, Clone)]
+            enum Intersection {
+                Corn(Corner),
+                Wind,
+            }
+
+            // Find all corners (and their kinds) and walls that occur
+            // at this Y coordinate and before this X
+            // coordinate. Those are the only intersecting points that
+            // matter if the element is inside or not.
+
+            let c = corners
+                .iter()
+                .filter(|(&(x, y), _)| y == rep.1 && x < rep.0)
+                .map(|((x, _), &c)| (x, Corn(c)));
+
+            let w = v_lines
+                .range(..=rep.0)
+                .filter(|(_, ys)| ys.iter().any(|ys| ys.contains(&rep.1)))
+                .map(|(x, _)| (x, Wind));
+
+            let mut intersections = c.chain(w).collect::<BTreeMap<_, _>>();
+
+            // Walk through the intersections two elements at a
+            // time. If we see a wall, toggle `inside` and put the
+            // second element back. If we see two corners that match,
+            // the shape doubled back and we don't change `inside`. If
+            // they are different, the shape kept going so toggle
+            // `inside`.
+            let mut inside = false;
+
+            loop {
+                match (intersections.pop_first(), intersections.pop_first()) {
+                    (None, None) => break,
+
+                    (Some((_, Wind)), v) => {
+                        inside = !inside;
+                        intersections.extend(v);
+                    }
+
+                    (Some((_, Corn(Upper))), Some((_, Corn(Upper))))
+                    | (Some((_, Corn(Lower))), Some((_, Corn(Lower)))) => {
+                        // no-op
+                    }
+
+                    (Some((_, Corn(Upper))), Some((_, Corn(Lower))))
+                    | (Some((_, Corn(Lower))), Some((_, Corn(Upper)))) => {
+                        inside = !inside;
+                    }
+
+                    o => unreachable!("Didn't handle {o:?}"),
+                }
+            }
+
+            if inside {
+                inside_filled += a;
+            }
+        }
+    }
+
+    assert_eq!(linear, outline_filled);
+
+    outline_filled + inside_filled
 }
 
 #[derive(Debug, Snafu)]
@@ -35,215 +268,36 @@ enum Error {
         source: ParseStepError,
         step: String,
     },
-
-    Bounds,
 }
 
-fn fill_in_outline(map: &Map, bounds: Bounds) -> BTreeSet<(i32, i32)> {
-    #[derive(Debug, Copy, Clone)]
-    enum Inside {
-        Yes,
-        No,
-    }
-
-    impl Inside {
-        fn toggle(&self) -> Inside {
-            match self {
-                Inside::Yes => Inside::No,
-                Inside::No => Inside::Yes,
-            }
-        }
-    }
-
-    #[derive(Debug, Copy, Clone)]
-    enum State {
-        Known(Inside),
-        Pending(Wall, Inside),
-    }
-
-    #[derive(Debug, Copy, Clone, PartialEq, Eq)]
-    enum Wall {
-        UpperBend,
-        Flat,
-        LowerBend,
-    }
-
-    let Bounds {
-        x_min,
-        x_max,
-        y_min,
-        y_max,
-    } = bounds;
-
-    let mut next = map.clone();
-
-    // eprintln!("{}", MapView(&next, bounds));
-
-    for y in y_min..=y_max {
-        let mut state = State::Known(Inside::No);
-
-        for x in x_min..=x_max {
-            let us = map.contains(&(x, y));
-
-            if us {
-                let above = map.contains(&(x, y - 1));
-                let below = map.contains(&(x, y + 1));
-
-                let w = match (above, below) {
-                    (true, true) | (false, false) => Wall::Flat,
-                    (true, false) => Wall::UpperBend,
-                    (false, true) => Wall::LowerBend,
-                };
-
-                // eprint!("{state:?} => {w:?} => ");
-
-                state = match (state, w) {
-                    (State::Known(i), Wall::Flat) => State::Known(i.toggle()),
-                    (State::Known(i), w) => State::Pending(w, i),
-
-                    (State::Pending(..), Wall::Flat) => state,
-                    (State::Pending(ow, i), nw) => {
-                        if ow == nw {
-                            State::Known(i)
-                        } else {
-                            State::Known(i.toggle())
-                        }
-                    }
-                };
-
-                // eprintln!("{state:?}");
-            }
-
-            if let State::Known(Inside::Yes) = state {
-                next.insert((x, y));
-            }
-        }
-
-        // eprintln!("QQQ\n");
-        // eprintln!("{}", MapView(&next, bounds));
-    }
-
-    next
-}
-
-struct MapView<'a>(&'a Map, Bounds);
-
-impl fmt::Display for MapView<'_> {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let Bounds {
-            x_min,
-            x_max,
-            y_min,
-            y_max,
-        } = self.1;
-
-        for y in y_min..=y_max {
-            for x in x_min..=x_max {
-                let c = if self.0.contains(&(x, y)) { '#' } else { '.' };
-                c.fmt(f)?;
-            }
-            "\n".fmt(f)?
-        }
-
-        Ok(())
-    }
-}
-
-#[derive(Debug, Copy, Clone)]
-struct Bounds {
-    x_min: i32,
-    x_max: i32,
-    y_min: i32,
-    y_max: i32,
-}
-
-impl Bounds {
-    fn initial(x: i32, y: i32) -> Self {
-        Self {
-            x_min: x,
-            x_max: x,
-            y_min: y,
-            y_max: y,
-        }
-    }
-
-    fn find(map: &Map) -> Option<Self> {
-        let mut b = None;
-
-        for &(x, y) in map {
-            let Bounds {
-                x_min,
-                x_max,
-                y_min,
-                y_max,
-            } = b.get_or_insert(Bounds::initial(x, y));
-
-            *x_min = cmp::min(x, *x_min);
-            *x_max = cmp::max(x, *x_max);
-            *y_min = cmp::min(y, *y_min);
-            *y_max = cmp::max(y, *y_max);
-        }
-
-        b
-    }
-}
-
-type Coord = (i32, i32);
-
-type Map = BTreeSet<Coord>;
-
-fn build_outline<'a>(steps: impl Iterator<Item = Step<'a>>) -> Map {
-    use direction_shorthand::*;
-
-    let mut x = 0;
-    let mut y = 0;
-
-    let mut map: Map = BTreeSet::new();
-
-    for Step { dir, count, .. } in steps {
-        match dir {
-            U => {
-                let next_y = y - count;
-                for c in (next_y..=y).map(|y| (x, y)) {
-                    map.insert(c);
-                }
-                y = next_y;
-            }
-
-            R => {
-                let next_x = x + count;
-                for c in (x..=next_x).map(|x| (x, y)) {
-                    map.insert(c);
-                }
-                x = next_x;
-            }
-
-            D => {
-                let next_y = y + count;
-                for c in (y..=next_y).map(|y| (x, y)) {
-                    map.insert(c);
-                }
-                y = next_y;
-            }
-
-            L => {
-                let next_x = x - count;
-                for c in (next_x..=x).map(|x| (x, y)) {
-                    map.insert(c);
-                }
-                x = next_x;
-            }
-        }
-    }
-
-    map
-}
-
+#[derive(Debug)]
 struct Step<'a> {
     dir: Direction,
-    count: i32,
-    #[allow(dead_code)]
+    count: i64,
     color: &'a str,
+}
+
+impl Step<'_> {
+    fn fixup(self) -> Step<'static> {
+        use direction_shorthand::*;
+
+        let (h, t) = self.color.split_at(5);
+
+        let count = i64::from_str_radix(h, 16).unwrap();
+        let dir = match t {
+            "0" => R,
+            "1" => D,
+            "2" => L,
+            "3" => U,
+            _ => panic!(),
+        };
+
+        Step {
+            dir,
+            count,
+            color: "",
+        }
+    }
 }
 
 impl<'a> TryFrom<&'a str> for Step<'a> {
@@ -259,7 +313,7 @@ impl<'a> TryFrom<&'a str> for Step<'a> {
         let dir = dir
             .parse::<Direction>()
             .context(InvalidDirectionSnafu { dir })?;
-        let count = count.parse::<i32>().context(InvalidCountSnafu { count })?;
+        let count = count.parse().context(InvalidCountSnafu { count })?;
 
         Ok(Self { dir, count, color })
     }
@@ -321,6 +375,14 @@ mod test {
     #[snafu::report]
     fn example_1() -> Result<(), Error> {
         assert_eq!(62, lava_volume(EXAMPLE_INPUT_1)?);
+
+        Ok(())
+    }
+
+    #[test]
+    #[snafu::report]
+    fn example_2() -> Result<(), Error> {
+        assert_eq!(952408144115, lava_volume_fixed(EXAMPLE_INPUT_1)?);
 
         Ok(())
     }
